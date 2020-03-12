9010dadc54573d0e3886aab051a88230b8b75554
yayanheeh <tokoxp58@gmail.com>
2020-03-12 11:19:56 +0700

    Update index.jsx

--- src/views/Dashboard/components/Status/index.jsx
+++ src/views/Dashboard/components/Status/index.jsx
@@ -9,7 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Badge from '@material-ui/core/Badge';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-
+import './App.css';
 const StyledBadge = withStyles(theme => ({
   badge: {
     backgroundColor: '#44b700',
@@ -62,6 +62,9 @@ export default function BadgeAvatars() {
   return (
     <div className={classes.root}>
 
+<div className="sticky">
+        
+
 <table width="100%" border="0 "><tr><td width="25%" bgcolor="red" align="left">Story</td>
 
 <td width="25%" bgcolor="" align="left"><StyledBadge
@@ -113,7 +116,7 @@ export default function BadgeAvatars() {
 
 </tr></table>
 
-
+</div>
       
     </div>
   );
